738cd9b12054e9ec26647ce38a6a1c2f5a6fe16b
abhiabhi74808-jpg <abhiabhi74808@gmail.com>
2026-07-28 23:59:24 +0530

    updated homepage
    code change in index , css , js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-document.getElementById("btn").addEventListener("click",function () {
-    alert("Welcome to Midnight Policy!");
-});
-
 function updateClock() {
     const now = new Date();
 
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+
     document.getElementById("clock").innerHTML =
-        now.toLocaleDateString("en-GB", {
-            weekday: "long",
-            day: "numeric",
-            month: "long",
-            year: "numeric"
-        }) +
+        now.toLocaleDateString("en-GB", options) +
         " | " +
-        now.toLocaleTimeString();
+        now.toLocaleTimeString("en-GB");
 }
 
 setInterval(updateClock, 1000);
@@ -21,26 +19,54 @@ updateClock();
 
 function searchNews() {
 
-    let search = document.getElementById("searchInput").value.toLowerCase();
+    let search =document.getElementById("searchInput")
+        .value
+        .trim()
+        .toLowerCase();
 
-    if (search === "india") {
+        if(search === ""){
+    alert("Please enter something to search.");
+    return;
+}
+
+    if (search.includes("india")) {
         window.location.href = "india.html";
     }
 
-    else if (search === "world") {
+    else if (search.includes("world")) {
         window.location.href = "world.html";
     }
 
-    else if (search === "economy") {
+    else if (search.includes("economy")) {
         window.location.href = "economy.html";
     }
 
-    else if (search === "defence") {
+    else if (search.includes("defence")) {
         window.location.href = "defence.html";
     }
 
+    else if (search.includes("research")) {
+        window.location.href = "research.html";
+    }
+
+    else if (search.includes("about")) {
+        window.location.href = "about.html";
+    }
+
+    else if (search.includes("contact")) {
+        window.location.href = "contact.html";
+    }
+
     else {
-        alert("No news found.");
+        alert("No news found.\nTry: India, World, Economy, Defence or Research.");
     }
 
 }
+
+document.getElementById("searchInput").addEventListener("keypress", function(event){
+
+    if(event.key === "Enter"){
+        searchNews();
+    }
+
+});
